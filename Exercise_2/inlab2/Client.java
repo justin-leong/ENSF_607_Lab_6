28c99d1fb7003a771078f2ba9a08d4af388b9e76
justin-leong <64918592+justin-leong@users.noreply.github.com>
2020-11-01 14:50:28 -0700

    Tested date-time client connection with DateServer and completed documentation for Ex2

--- Exercise_2/inlab2/Client.java
+++ Exercise_2/inlab2/Client.java
@@ -6,14 +6,38 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
+/***
+ * This class represents a client that sends a request for the date or time to a server and
+ * displays the response to the console. This client class communicates on port 9090 of the socket.
+ * 
+ * @author Justin Leong
+ * @version 1.0
+ * @since November 1, 2020
+ *
+ */
 public class Client {
-	
+	/**
+	 * The socket to communicate with the server
+	 */
 	private Socket socket;
+	/**
+	 * Output connection of socket
+	 */
 	private PrintWriter socketOut;
+	/**
+	 * Input connection of socket
+	 */
 	private BufferedReader socketIn;
+	/**
+	 * Input from console
+	 */
 	private BufferedReader stdIn;
 	
+	/**
+	 * Constructs a client that connects to a server on a desired port
+	 * @param serverName name of the server 
+	 * @param portNumber port to connect to
+	 */
 	public Client(String serverName, int portNumber) {
 		try {
 			socket = new Socket(serverName, portNumber);
@@ -27,6 +51,9 @@ public class Client {
 		}
 	}
 	
+	/**
+	 * Sends request to server and prints response to console
+	 */
 	public void communicate() {
 		
 		String selection = "";
@@ -35,12 +62,16 @@ public class Client {
 		
 		while(running) {
 			try {
+				// read input from console
 				System.out.println("Please select an option (DATE/TIME) or QUIT to exit");
 				selection = stdIn.readLine();
 				
+				// sends selection to server 
 				socketOut.println(selection);
+				// receives server response
 				response = socketIn.readLine();
 				
+				// displays response or ends client session depending on selection
 				if(!selection.equals("QUIT")) {
 					System.out.println(response);
 				} else {
@@ -51,6 +82,7 @@ public class Client {
 			}
 		}
 		
+		// closes appropriate sockets to end connection
 		try {
 			socketIn.close();
 			socketOut.close();
@@ -59,7 +91,13 @@ public class Client {
 		}
 	}
 	
-	public static void main(String[] args) {
+	/**
+	 * Run the client
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
 		Client myClient = new Client("localhost", 9090);
 		myClient.communicate();
 	}
