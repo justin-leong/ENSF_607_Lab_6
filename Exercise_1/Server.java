7dadf177b91adfd3d28c4e773d4c1500ef27cbed
justin-leong <64918592+justin-leong@users.noreply.github.com>
2020-11-01 11:25:45 -0700

    Developed algorithm to check if a string is a palindrome in the Server class for Ex1

--- Exercise_1/Server.java
+++ Exercise_1/Server.java
@@ -20,7 +20,43 @@ public class Server {
 		}
 	}
 	
+	public void communicate() {
+		String line = null;
+		
+		while(true) {
+			try {
+				line = socketIn.readLine();
+
+				if(line == null) {
+					break;
+				}
+				
+				boolean isPalindrome = palindrome(line);
+				if(isPalindrome) {
+					socketOut.println(line + " is a Palindrome.");
+				} else {
+					socketOut.println(line + " is not a Palindrome.");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
+	// function to check if a string is a palindrome
+	public boolean palindrome(String line) {
+		// number of characters in string
+		int n = line.length();
+		
+		// compare character pairs to check whether string is a palindrome
+		for(int i = 0; i < n/2; i++) {
+			if(line.charAt(i) != line.charAt(n-1-i)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
 	
 	public static void main(String[] args) throws IOException {
 		Server myServer = new Server(8099);
@@ -33,6 +69,8 @@ public class Server {
 			
 			myServer.socketIn = new BufferedReader(new InputStreamReader(myServer.palinSocket.getInputStream()));
 			myServer.socketOut = new PrintWriter(myServer.palinSocket.getOutputStream(), true);
+			
+			myServer.communicate();
 			
 			myServer.socketIn.close();
 			myServer.socketOut.close();
